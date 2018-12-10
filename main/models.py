61e5a9f8b5fdf9e3367429182e4965ec3043ed62
almas <mendygax@gmail.com>
2018-12-09 21:34:23 -0500

    updating django admin model

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length = 1200)
     description = models.TextField(default="description default")
+    location = models.CharField(max_length = 1200, default = "my location")
 
-def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
