fa9a92ca772ba51064704e3e35149b11d5ce3593
almas <mendygax@gmail.com>
2018-12-11 23:28:31 -0500

    testing makemigrations

--- main/models.py
+++ main/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length = 1200)
     description = models.TextField(default="description default")
-    location = models.CharField(max_length = 1200, default = "my location")
+    location = models.CharField(max_length = 1200, default = "my location", blank=True)
+    job = models.CharField(max_length = 1200, null=True)
 
     def __unicode__(self):
         return self.name
